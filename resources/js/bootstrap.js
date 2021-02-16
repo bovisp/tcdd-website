0c7b90943974470210d5f44514e99a04865d99e1
Bovis <Paul.Bovis2@ec.gc.ca>
2021-02-16 11:35:13 -0700

    Assign the correct pusher auth endpoint depending upon node environment

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -13,15 +13,18 @@ import Echo from 'laravel-echo';
 window.Pusher = require('pusher-js');
 
 let pusherKey = ''
+let authEndpoint = ''
 
 if (process.env.NODE_ENV === 'production') {
     pusherKey = '527ffffc14a075ceaf06'
+    authEndpoint = 'http://msc-educ-smc.cmc.ec.gc.ca/tcdd-website/broadcasting/auth'
 } else if (process.env.NODE_ENV === 'development') {
     pusherKey = 'd5dc29f415486ec427d3'
+    authEndpoint = 'http://localhost:8000/broadcasting/auth'
 }
 
 window.Echo = new Echo({
-    authEndpoint : process.env.MIX_PUSHER_APP_AUTH_ROUTE,
+    authEndpoint,
     broadcaster: 'pusher',
     key: pusherKey,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
