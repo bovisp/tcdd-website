60ec2866e33be245258988af25043e34d66c4b5d
Bovis <Paul.Bovis2@ec.gc.ca>
2021-06-16 14:05:40 -0600

    Translating js/pages/questions/questions Vue files - Part 2

---
+++ resources/lang/en/js_pages_questions_questions_questionsindex.php
@@ -0,0 +1,10 @@
+<?php
+
+return array (
+  'questions' => 'Questions',
+  'edit' => 'Edit',
+  'noquestions' => 'No questions have been created.',
+  'category' => 'Category',
+  'section' => 'Section',
+  'name' => 'Name',
+);
---
+++ resources/lang/fr/js_pages_questions_questions_questionsindex.php
@@ -0,0 +1,10 @@
+<?php
+
+return array (
+  'questions' => 'f Questions',
+  'edit' => 'f Edit',
+  'noquestions' => 'f No questions have been created.',
+  'category' => 'f Category',
+  'section' => 'f Section',
+  'name' => 'f Name',
+);
