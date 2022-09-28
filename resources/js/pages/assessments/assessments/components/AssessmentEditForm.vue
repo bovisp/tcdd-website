31aa9154ddc4eb12c0c91cacf7e3924123fa92a9
Paul Bovis <paulbovisblahblah@gmail.com>
2022-09-28 09:46:28 -0600

    Populate form error pt 9

--- resources/js/pages/assessments/assessments/components/AssessmentEditForm.vue
+++ resources/js/pages/assessments/assessments/components/AssessmentEditForm.vue
@@ -254,19 +254,20 @@ export default {
     async mounted () {
         await this.fetchSections()
         await this.fetchTypes()
-        await this.populateForm()
+        // await this.populateForm()
 
-        let emptyProps = []
+        // let emptyProps = []
 
         for (let prop in this.form) {
-            if (!this.form[prop]) {
-                this.form[prop] = this.assessment[prop]
-                console.log(`assessment: ${this.assessment[prop]} and form ${this.form[prop]} `)
-            }
+            // if (!this.form[prop]) {
+                this.form[prop] = this.$store.state.assessments.assessment[prop]
+                // console.log(`assessment: ${this.assessment[prop]} and form ${this.form[prop]} `)
+            // }
         }
 
         // console.log(emptyProps)
-        console.log(this.form)
+        // console.log(this.form)
+        // console.log(this.$store.state.assessments.assessment)
     }
 }
 </script>
